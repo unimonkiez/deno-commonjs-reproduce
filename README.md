71675917e0c11b0b4f9cbe2d229ae623fe10179c
Yuval Saraf <unimonkiez@gmail.com>
2024-11-27 15:14:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Related to issue [#27079](https://github.com/denoland/deno/issues/27079) of Deno  
+Run in terminal
+
+```bash
+deno run dev
+```
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+// @deno-types="./generated-code/index.d.ts"
+import * as mod from "./generated-code/index.js";
+
+console.log(`Imported the value of "a" which is "${mod.a}"`);
